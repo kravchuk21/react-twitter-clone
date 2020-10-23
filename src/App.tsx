080b5533770647cd4eb5fe2eec33f2956591b6dd
kravchuk21 <krauchukvlad@gmail.com>
2020-10-23 20:46:17 +0300

    Урок №9

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { Layout } from './pages/Layout';
 import { SignIn } from './pages/SignIn';
+import { UserPage } from './pages/User';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/signin" component={SignIn} />
-        <Route path="/" component={Home} />
+        <Route path="/signin" component={SignIn} exact />
+        <Layout>
+          <Route path="/home" component={Home} />
+          <Route path="/user" component={UserPage} />
+        </Layout>
       </Switch>
     </div>
   );
--- src/pages/Home/components/FullTweet.tsx
+++ src/pages/Home/components/FullTweet.tsx
@@ -1,14 +1,22 @@
+import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import classNames from 'classnames';
-import React from 'react';
+import CommentIcon from '@material-ui/icons/ChatBubbleOutlineOutlined';
+import RepostIcon from '@material-ui/icons/RepeatOutlined';
+import LikeIcon from '@material-ui/icons/FavoriteBorderOutlined';
+import ShareIcon from '@material-ui/icons/ReplyOutlined';
+import { Divider, IconButton } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import format from 'date-fns/format'
+import ruLang from 'date-fns/locale/ru'
 import { fetchTweetData, setTweetData } from '../../../store/ducks/tweet/actionCreators';
 import { selectIsTweetLoading, selectTweetData } from '../../../store/ducks/tweet/selectors';
-import { useHomeStyles } from '../theme';
+import { useHomeStyles } from '../../theme';
+import { Tweet } from '../../../components/Tweet';
 
 export const FullTweet: React.FC = (): React.ReactElement | null => {
   const classes = useHomeStyles();
@@ -38,26 +46,81 @@ export const FullTweet: React.FC = (): React.ReactElement | null => {
 
   if (tweetData) {
     return (
-      <Paper className={classes.fullTweet}>
-        <div className={classNames(classes.tweetsHeaderUser)}>
-          <Avatar
-            className={classes.tweetAvatar}
-            alt={`Аватарка пользователя ${tweetData.user.fullname}`}
-            src={tweetData.user.avatarUrl}
-          />
-          <Typography>
-            <b>{tweetData.user.fullname}</b>&nbsp;
-            <div>
-              <span className={classes.tweetUserName}>@{tweetData.user.username}</span>&nbsp;
-              <span className={classes.tweetUserName}>·</span>&nbsp;
-              <span className={classes.tweetUserName}>1 ч</span>
-            </div>
+      <>
+        <Paper className={classes.fullTweet}>
+          <div className={classNames(classes.tweetsHeaderUser)}>
+            <Avatar
+              className={classes.tweetAvatar}
+              alt={`Аватарка пользователя ${tweetData.user.fullname}`}
+              src={tweetData.user.avatarUrl}
+            />
+            <Typography>
+              <b>{tweetData.user.fullname}</b>&nbsp;
+              <div>
+                <span className={classes.tweetUserName}>@{tweetData.user.username}</span>&nbsp;
+              </div>
+            </Typography>
+          </div>
+          <Typography className={classes.fullTweetText} gutterBottom>
+            {tweetData.text}
           </Typography>
-        </div>
-        <Typography className={classes.fullTweetText} gutterBottom>
-          {tweetData.text}
-        </Typography>
-      </Paper>
+          <Typography>
+            <span className={classes.tweetUserName}>{format(new Date(tweetData.createdAt), 'H:mm', { locale: ruLang })} · </span>
+            <span className={classes.tweetUserName}>{format(new Date(tweetData.createdAt), 'dd MMM. yyyy г.', { locale: ruLang })}</span>
+          </Typography>
+          <div className={classNames(classes.tweetFooter, classes.fullTweetFooter)}>
+            <IconButton>
+              <CommentIcon style={{ fontSize: 25 }} />
+            </IconButton>
+            <IconButton>
+              <RepostIcon style={{ fontSize: 25 }} />
+            </IconButton>
+            <IconButton>
+              <LikeIcon style={{ fontSize: 25 }} />
+            </IconButton>
+            <IconButton>
+              <ShareIcon style={{ fontSize: 25 }} />
+            </IconButton>
+          </div>
+        </Paper>
+        <Divider />
+        <Tweet
+          _id="1"
+          text="Any more to move? You might need to adjust your stretching routines!"
+          createdAt={new Date().toString()}
+          user={{
+            fullname: 'Arlene Andrews',
+            username: 'ArleneAndrews_1',
+            avatarUrl:
+              'https://pbs.twimg.com/profile_images/1172922412029136897/gFRmgn1W_bigger.jpg',
+          }}
+          classes={classes}
+        />
+        <Tweet
+          _id="1"
+          text="Any more to move? You might need to adjust your stretching routines!"
+          createdAt={new Date().toString()}
+          user={{
+            fullname: 'Arlene Andrews',
+            username: 'ArleneAndrews_1',
+            avatarUrl:
+              'https://pbs.twimg.com/profile_images/1172922412029136897/gFRmgn1W_bigger.jpg',
+          }}
+          classes={classes}
+        />
+        <Tweet
+          _id="1"
+          text="Any more to move? You might need to adjust your stretching routines!"
+          createdAt={new Date().toString()}
+          user={{
+            fullname: 'Arlene Andrews',
+            username: 'ArleneAndrews_1',
+            avatarUrl:
+              'https://pbs.twimg.com/profile_images/1172922412029136897/gFRmgn1W_bigger.jpg',
+          }}
+          classes={classes}
+        />
+      </>
     );
   }
 
